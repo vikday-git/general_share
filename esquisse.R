b030f491140450cbbf8cfc60cea5ed63acdcc62b
vikday-git <69419827+vikday-git@users.noreply.github.com>
2021-09-16 08:39:48 +0530

    Add files via upload

---
+++ esquisse.R
@@ -0,0 +1,16 @@
+
+
+# read in data into R
+
+# install esquisse
+install.packages("esquisse")
+
+# load esquisse
+library(esquisse)
+
+# open dialogue box
+esquisser(owid_ies)
+
+# make graph as you want
+
+# copy and paste code, or save graph
